3ba97d226bd7cefe82175ddad6a5dc9b5dceef8d
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-01 13:05:06 -0500

    add core js functionality

--- script.js
+++ script.js
@@ -16,21 +16,26 @@ function addBookToLibrary(){
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
-
+    
     const a = new Book(title,author,pages);
     myLibrary.push(a);
-  
+    
     showBooks();
 }
 
 function showBooks(){
     let output = document.querySelector(".output");
-    myLibrary.forEach(e => output.innerHTML += 
-    "<div>" + e.title + "<button>X</button></div>");
+    output.innerHTML = "";
+    myLibrary.forEach(function (e,i) {output.innerHTML += 
+    `<div>${e.title}, ${e.author}, ${e.pages} left. <button onclick = "removeBook(${i})" class = "remove">X</button></div>`});
+    
 }
 
+function removeBook(id){
+    myLibrary.splice(id,1);
+    showBooks();
+}
 
+let add = document.querySelector(".add");
 
-let btn = document.querySelector(".add");
-btn.addEventListener("click",addBookToLibrary);
-
+add.addEventListener("click",addBookToLibrary);
