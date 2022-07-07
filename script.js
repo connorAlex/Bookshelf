356e5a358095b7eed2e2f7c1af78cf102ed2e7f9
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-07 10:27:24 -0500

    add off/on mouseover events

--- script.js
+++ script.js
@@ -13,41 +13,100 @@ function Book(title, author, pages){
     }
 }
 
+//add book to an array
 function addBookToLibrary(){
     let data = validate();
+
     if (data === false){
         return 0;
     }
-    const a = new Book(data.title,data.author,data.pages);
-    myLibrary.push(a);
+
+    const newBook = new Book(data.title,data.author,data.pages);
+    checkRead(data.read);
+    myLibrary.push(newBook);
+
+
+    function checkRead(isBookRead) {
+        if (isBookRead === "Yes") {
+            newBook.hasRead = true;
+        }
+    }
+
     showBooks();
 }
 
+//update DOM with all books in myLibrary array
 function showBooks(){
     let output = document.querySelector(".output");
     output.innerHTML = "";
-    myLibrary.forEach(function (e,i) {output.innerHTML += 
-    `<div><div>${e.title}</div> <div>${e.author}</div> <div>${e.pages} left</div> <div>read?</div><button onclick = "removeBook(${i})" class = "remove">X</button></div>`});
-    
+    myLibrary.forEach(function (e,i) {
+        let readOutput = "Unread";
+        
+        if (e.hasRead === true){
+            readOutput = "Have Read";
+        }
+
+        output.innerHTML =  
+            `<div>   
+                <div>${e.title}</div> 
+                <div>${e.author}</div> 
+                <div>${e.pages} left</div>
+                <div>${readOutput}</div>
+                <button 
+                        onclick = "removeBook(${i})" 
+                        class = "remove">
+                            X
+                </button>
+            </div>` + output.innerHTML;
+        }
+    );    
 }
 
+//remove a specific book from the stack
 function removeBook(id){
     myLibrary.splice(id,1);
     showBooks();
 }
 
+// js validation for the different input elements
+// recreating the <form> functionality here... not good...
 function validate(){
+
+    //these vars can be condensed to one line
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
 
-    let data = [title, author, pages];
+    //let a = document.getElementsByTagName("input");
+    
+    let values = [title, author, pages ,read];
+    let data = values;
     for (let i = 0; i < data.length; i++){
         if (data[i] === ""){
             return false;
         }
     }
-    return {'title':data[0], 'author':data[1], 'pages':data[2]};
+    reset(values);
+    return {'title':data[0], 'author':data[1], 'pages':data[2], 'read':data[3]};
 }
+
+//reset the input values to empty string
+function reset(values){
+    values.forEach(e => e = "");
+}
+
 let add = document.querySelector(".add");
 add.addEventListener("click",addBookToLibrary);
+
+let addBook = document.querySelector(".addBook");
+addBook.addEventListener("mouseenter",on);
+addBook.addEventListener("mouseleave",off);
+
+function on(e){
+    document.querySelector(".entry").style.display = "flex";
+}
+
+function off(e){
+    document.querySelector(".entry").style.display = "none";
+}
